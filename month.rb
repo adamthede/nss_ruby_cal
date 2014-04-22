f42a8a9d5c387e43e18bcc6b08a682300f4d412b
Adam Thede <adam@thedetech.com>
2014-04-22 11:34:16 -0500

    major modifications to month.rb and year.rb

--- month.rb
+++ month.rb
@@ -26,8 +26,6 @@ class Month
     end
     # new construction of the algorithm
     day_on_first_of_month = (1 + ((26 *(month + 1))/10).floor + year + (year/4).floor + (6*(year/100)).floor + (year/400).floor) % 7
-    # potentially incorrect algorithm
-    # day_on_first_of_month = (1 + (13*(@month+1))/5 + (@year % 100) + ((@year % 100)/4) + 5 * (@year / 100)) % 7
     return day_on_first_of_month
   end
 
@@ -84,4 +82,60 @@ class Month
     puts "Su Mo Tu We Th Fr Sa"
     puts construct_month
   end
+
+  def print_month_to_array
+    month_array = []
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_text = months[@month - 1]
+    title = "#{month_text}"
+    month_array << title.center(20)
+    month_array << "Su Mo Tu We Th Fr Sa"
+    month_array << construct_month
+    return month_array
+  end
+
+  def construct_month_array
+    month_days = days_in_month
+    first_day = day_on_first_of_month
+    if first_day == 0
+      calendar_fill = 6
+    else
+      calendar_fill = first_day - 1
+    end
+    calendar = []
+    calendar_fill.times do
+      calendar << 0
+    end
+    rest_of_calendar = (1..month_days).to_a
+    calendar += rest_of_calendar
+    new_calendar = calendar.each_slice(7).to_a
+    final_calendar = []
+    new_calendar.each do |row|
+      calendar_string = ""
+      row.each do |day|
+        if day == 0
+          calendar_string += "  "
+        elsif day < 10
+          calendar_string += (" "+day.to_s)
+        else
+          calendar_string += day.to_s
+        end
+        calendar_string += " "
+      end
+      final_calendar << calendar_string.chomp(" ")
+    end
+    new_final_calendar = []
+    final_calendar.each do |line|
+      new_final_calendar << line.ljust(20)
+    end
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_text = months[@month - 1]
+    new_final_calendar.unshift("Su Mo Tu We Th Fr Sa")
+    new_final_calendar.unshift("#{month_text}".center(20))
+    if new_final_calendar.length == 7
+      new_final_calendar.push("                    ")
+    end
+    return new_final_calendar
+  end
+
 end
--- year.rb
+++ year.rb
@@ -1,4 +1,49 @@
 class Year
   require_relative 'month.rb'
 
-  
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def construct_year
+    year_display = []
+    12.times do |i|
+      year_display << Month.new(i+1, @year).construct_month_array
+    end
+    return year_display
+  end
+
+  def print_year
+    year_screen = construct_year.each_slice(3).to_a
+
+    year_display = []
+    year_screen.each do |item|
+      i = 0
+      until item[0][i].nil? && item[1][i].nil? && item[2][i].nil?
+=begin
+        if item[0][i].nil? && !item[1][i].nil? && !item[2][i].nil?
+          year_display << "                    " + "  " + item[1][i] + "  " + item[2][i]
+        elsif item[0][i].nil? && item[1][i].nil? && !item[2][i].nil?
+          year_display << "                    " + "  " + "                    " + "  " + item[2][i]
+        elsif item[0][i].nil? && !item[1][i].nil? && item[2][i].nil?
+          year_display << "                    " + "  " + item[1][i] + "  " + "                    "
+        elsif !item[0][i].nil? && item[1][i].nil? && item[2][i].nil?
+          year_display << item[0][i] + "  " + "                    " + "  " + "                    "
+        elsif !item[0][i].nil? && !item[1][i].nil? && item[2][i].nil?
+          year_display << item[0][i] + "  " + item[1][i] + "  " + "                    "
+        else
+=end
+          year_display << item[0][i] + "  " + item[1][i] + "  " + item[2][i]
+        # end
+        i += 1
+      end
+    end
+    year_display.unshift("\n")
+    year_display.unshift("#{@year}".center(62))
+    year_display.each do |item|
+      puts item
+    end
+  end
+end
