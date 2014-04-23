97c418e8b755405cbc9330aed091bef84575b65d
Adam Thede <adam@thedetech.com>
2014-04-22 22:43:57 -0500

    added errors for exceptions and a readme :clap:

--- cal.rb
+++ cal.rb
@@ -4,17 +4,21 @@ require_relative 'year.rb'
 if ARGV.length == 1
   if ARGV[0].length > 2
     year = ARGV[0].to_i
+    abort ("please enter a year between 1800 and 3000") if year < 1800 || year > 3000
     cal = Year.new(year)
     cal.to_s
   else
     year = 2014
     month = ARGV[0].to_i
+    abort ("please enter a month between 1 and 12") if month < 1 || month > 12
     cal = Month.new(month, year)
     cal.print_month_to_screen
   end
 else
   month = ARGV[0].to_i
   year = ARGV[1].to_i
+  abort ("please enter a month between 1 and 12") if month < 1 || month > 12
+  abort ("please enter a year between 1800 and 3000") if year < 1800 || year > 3000
   cal = Month.new(month, year)
   cal.print_month_to_screen
 end
